abea01fa9942a368881a51d4ba2ba5d32beec58e
agrawalsrijan <47100634+agrawalsrijan@users.noreply.github.com>
2019-10-18 21:05:30 +0530

    Refactoring Routes into a new Component
    Creating a seperate Component for Routes.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import DogList from "./DogList";
-import DogDetails from "./DogDetails";
+
 import Navbar from "./Navbar";
+import Routes from "./Routes";
 import hazel from "./imgs/hazel.jpg";
 import tubby from "./imgs/tubby.jpg";
 import whiskey from "./imgs/whiskey.jpg";
-import {Switch,Route} from "react-router-dom";
+
 import './App.css';
 
 class App extends Component {
@@ -44,21 +44,11 @@ class App extends Component {
     ]
   };
   render() {
-    // logic to get specific dog
-    const getDog = props => {
-      let name = props.match.params.name;
-      let currentDog = this.props.dogs.find(
-        dog => dog.name.toLowerCase() === name.toLowerCase()
-      );
-      return <DogDetails {...props} dog={currentDog}/>
-    }
+
     return(
       <div>
         <Navbar dogs={this.props.dogs}/>
-        <Switch>
-          <Route exact path="/dogs" render={()=><DogList dogs={this.props.dogs}/>} />;
-          <Route exact path="/dogs/:name" render={(getDog)} />;
-        </Switch>
+        <Routes dogs={this.props.dogs}/>
       </div>
 
 
